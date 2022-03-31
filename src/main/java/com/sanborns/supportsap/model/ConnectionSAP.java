9c63d7dabb2a7ae724fc931eedead09c86d25569
joscarv <juarezo@sanborns.clvr>
2022-03-31 08:38:04 -0600

    Se agrega Timeout's a URL de conexion SQL Server SAP

--- src/main/java/com/sanborns/supportsap/model/ConnectionSAP.java
+++ src/main/java/com/sanborns/supportsap/model/ConnectionSAP.java
@@ -4,6 +4,7 @@ public class ConnectionSAP extends Crud
 {	
 	public ConnectionSAP(String host, String database)
 	{
-		super("jdbc:sqlserver://" + host + ":1433;databaseName=" + database,"sa","Sa@p0$d3$");
-	}	
+		super("jdbc:sqlserver://" + host + ":1433;databaseName=" + database +
+				";lockTimeout=15;queryTimeout=15;socketTimeout=15000","sa","Sa@p0$d3$");
+	}
 }
