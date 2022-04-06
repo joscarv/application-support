1d93a18ab3798cfb6769e849213262265059e683
OSCAR JUAREZ VEGA <juarezo@sanborns.clvr>
2022-04-05 19:50:31 -0500

    Se modifica conexion sql singleton

--- src/main/java/com/sanborns/supportsap/model/Connection24.java
+++ src/main/java/com/sanborns/supportsap/model/Connection24.java
@@ -3,16 +3,18 @@ package com.sanborns.supportsap.model;
 public class Connection24 extends Crud
 {	
 	private static Connection24 instance = null;
+	private static boolean isConnected;
 	
 	private Connection24()
 	{
 		super("jdbc:sqlserver://10.128.10.24:1433;databaseName=Db_Util", "SapPsi", "kTIX3wxO8?");
+		isConnected = getIsConnected();
 		System.out.println("Inicia una instancia de Connection24");
 	}
 	
 	public static synchronized Connection24 getInstance()
 	{
-		if( instance == null)
+		if( instance == null || !isConnected )
 		{
 			instance = new Connection24();
 		}
--- src/main/java/com/sanborns/supportsap/model/Connection59.java
+++ src/main/java/com/sanborns/supportsap/model/Connection59.java
@@ -3,15 +3,18 @@ package com.sanborns.supportsap.model;
 public class Connection59 extends Crud 
 {
 	private static Connection59 instance;
+	private static boolean isConnected;
 	
 	private Connection59()
 	{
 		super("jdbc:sqlserver://10.128.10.59:1433;databaseName=SalesAuditSears", "SapPsi", "Pa$$w0rd");
+		isConnected = getIsConnected();
+		System.out.println("Inicia una instancia de Connection59");
 	}
 	
 	public static synchronized Connection59 getInstance()
 	{
-		if( instance == null)
+		if( instance == null || !isConnected )
 		{
 			instance = new Connection59();
 		}
