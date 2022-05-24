db3954099efb6f76ffb61349ac5bf183c78d5d55
Oscar Juarez Work <juarezo@sanborns.clvr>
2022-05-24 16:57:53 -0500

    Se leen parametros FTP desde .config.conf

--- src/main/java/com/sanborns/supportsap/utils/Config.java
+++ src/main/java/com/sanborns/supportsap/utils/Config.java
@@ -10,7 +10,7 @@ import java.util.Properties;
 
 public class Config 
 {
-	private final String pathFile = "./config.conf";
+	private final String pathFile = "./.config.conf";
 	private Properties properties;
 	private static InputStream inStream;
 	private static Config instance;
@@ -69,5 +69,75 @@ public class Config
 		}
 		e = null;
 		return list;
+	}
+
+	public String getServerFTP41()
+	{
+		if(inStream == null)
+			return "";
+		return properties.getProperty("serverFTP41");
+	}
+
+	public String getUserSanb41()
+	{
+		if(inStream == null)
+			return "";
+		return properties.getProperty("userSanb41");
+	}
+
+	public String getPassSanb41()
+	{
+		if(inStream == null)
+			return "";
+		return properties.getProperty("passSanb41");
+	}
+
+	public String getUserCafe41()
+	{
+		if(inStream == null)
+			return "";
+		return properties.getProperty("userCafe41");
+	}
+
+	public String getPassCafe41()
+	{
+		if(inStream == null)
+			return "";
+		return properties.getProperty("passCafe41");
+	}
+
+	public String getServerFTP24()
+	{
+		if(inStream == null)
+			return "";
+		return properties.getProperty("serverFTP24");
+	}
+
+	public String getUserFTP24()
+	{
+		if(inStream == null)
+			return "";
+		return properties.getProperty("userFTP24");
+	}
+
+	public String getPassFTP24()
+	{
+		if(inStream == null)
+			return "";
+		return properties.getProperty("passFTP24");
+	}
+
+	public String getUserSanb_bkp()
+	{
+		if(inStream == null)
+			return "";
+		return properties.getProperty("userSanb41_bkp");
+	}
+
+	public String getPassSanb_bkp()
+	{
+		if(inStream == null)
+			return "";
+		return properties.getProperty("passSanb41_bkp");
 	}
 }
