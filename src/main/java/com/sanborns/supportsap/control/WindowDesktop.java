37b56400687e9a418ed715b863e75cd29c49fc10
Oscar Juarez Work <juarezo@sanborns.clvr>
2022-05-25 09:23:35 -0500

    Se agregan iconos para menus

--- src/main/java/com/sanborns/supportsap/control/WindowDesktop.java
+++ src/main/java/com/sanborns/supportsap/control/WindowDesktop.java
@@ -1,10 +1,12 @@
 package com.sanborns.supportsap.control;
 
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
@@ -33,10 +35,12 @@ public class WindowDesktop extends JFrame
 	private JMenuItem jmiIISanborns41;
 	
 	// VAR INTERNALFRAME NIGHT REVIEW 
-	InterfaceReview iSalesWeb;
-	InterfaceReview iOpenStore;
-	InterfaceReview iEodProc;
-	InterfaceReview iInterfacesSan41;
+	private InterfaceReview iSalesWeb;
+	private InterfaceReview iOpenStore;
+	private InterfaceReview iEodProc;
+	private InterfaceReview iInterfacesSan41;
+
+	private Font font;
 	
 	public WindowDesktop()
 	{
@@ -50,27 +54,43 @@ public class WindowDesktop extends JFrame
 	
 	private void initComponents()
 	{
+		setIconImage(new ImageIcon("./img/sabiduria.png").getImage());
 		// DESKTOPPANE
 		desktopPane = new JDesktopPane();
 		setContentPane(desktopPane);
 		
+		font = new Font("Arial",Font.BOLD,12);
 		// INITIALIZE MENUITEM NIGHT REVIEW
 		jmiISalesWeb = new JMenuItem("Ventas Internet");
 		jmiISalesWeb.addActionListener(actionListener());
+		jmiISalesWeb.setFont(font);
+		jmiISalesWeb.setIcon(new ImageIcon("./img/www.png"));
 		jmiIOpenStore = new JMenuItem("Unidades Abiertas");
 		jmiIOpenStore.addActionListener(actionListener());
+		jmiIOpenStore.setFont(font);
+		jmiIOpenStore.setIcon(new ImageIcon("./img/open.png"));
 		jmiIEodProc = new JMenuItem("Inicio de Eodproc");
 		jmiIEodProc.addActionListener(actionListener());
+		jmiIEodProc.setFont(font);
+		jmiIEodProc.setIcon(new ImageIcon("./img/start.png"));
 		jmiIISanborns41 = new JMenuItem("Interfaces Sanborns 41");
 		jmiIISanborns41.addActionListener(actionListener());
+		jmiIISanborns41.setFont(font);
+		jmiIISanborns41.setIcon(new ImageIcon("./img/fourty-one.png"));
 		
+		font = new Font("Arial",Font.BOLD,13);
 		// INITIALIZE JMENU
 		jmSupport = new JMenu("Soporte");
+		jmSupport.setFont(font);
+		jmSupport.setIcon(new ImageIcon("./img/support.png"));
 		jmReview = new JMenu("Guardia Nocturna");
+		jmReview.setFont(font);	
+		jmReview.setIcon(new ImageIcon("./img/review.png"));
 		jmReview.add(jmiISalesWeb);
 		jmReview.add(jmiIOpenStore);
 		jmReview.add(jmiIEodProc);
 		jmReview.add(jmiIISanborns41);
+		
 		
 		// INITIALIZE MENUBAR
 		menuBar = new JMenuBar();
@@ -87,7 +107,7 @@ public class WindowDesktop extends JFrame
 		this.add(iEodProc);
 		iInterfacesSan41 = new InternalSanborns41();
 		this.add(iInterfacesSan41);
-	}
+	}	
 	
 	private void setLocationJFrame()
 	{
@@ -112,7 +132,7 @@ public class WindowDesktop extends JFrame
 				if(e.getSource() == jmiIEodProc)
 					iEodProc.setVisible(true);
 				if(e.getSource() == jmiIISanborns41)
-					iInterfacesSan41.setVisible(true);
+					iInterfacesSan41.setVisible(true);	
 			}
 		};
 	}
